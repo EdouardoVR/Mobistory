0db177aba952be533eb5a1499926d99aac7e4249
Edouardo Rodrigues <vilarinhorodri@edu.univ-eiffel.fr>
2024-03-01 15:12:39 +0100

    initial project version

---
+++ app/src/main/java/fr/uge/mobistorytra/Main.kt
@@ -0,0 +1,26 @@
+package fr.uge.mobistorytra
+
+
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
+
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
